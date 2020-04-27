1e052d09c122100a5280b35e2e4614534f8953e1
Brittany Graham <britt.p.graham@gmail.com>
2020-04-26 22:42:32 -0600

    cpp code

---
+++ cmake-build-debug/CMakeFiles/ProgramLangProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProgramLangProject.dir/main.cpp.obj"
+  "ProgramLangProject.exe"
+  "ProgramLangProject.exe.manifest"
+  "ProgramLangProject.pdb"
+  "libProgramLangProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProgramLangProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+auto SelectionSort(int A[], int n){
+    auto start = high_resolution_clock::now();
+    for (int i = 0; i < n-1; ++i) {
+        int first = i;
+        int minimum = first;
+        for (int j = i + 1; j < n; ++j) {
+            if(A[j] < A[minimum]){
+                minimum = j;
+            }
+        }
+        if (minimum != first){
+            int temp = A[minimum];
+            A[minimum] = A[i];
+            A[i] = temp;
+        }
+    }
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+    return duration;
+}
+
+auto BubbleSort(int A[], int n){
+    auto start = high_resolution_clock::now();
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < (n-i); ++j) {
+            if (A[j-1] > A[j]){
+                int temp = A[j-1];
+                A[j-1] = A[j];
+                A[j] = temp;
+            }
+        }
+    }
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+    return duration;
+}
+
+void merge(int A[], int low, int mid, int high){
+    int b[(mid-low) + 1];
+    int c[(high - mid)];
+    for (int i = 0; i < sizeof(b); ++i) {
+        b[i] = A[low + i];
+    }
+    for (int j = 0; j < sizeof(c); ++j) {
+        c[j] = A[mid + 1 + j];
+    }
+    int k = low;
+    int i = 0;
+    int j = 0;
+    while (i < sizeof(b) && j < sizeof(c)){
+        if(b[i] < c[j]){
+            A[k] = b[i];
+            i++;
+            k++;
+        }
+        else{
+            A[k] = c[j];
+            j++;
+            k++;
+        }
+    }
+    if( i == sizeof(b)) {
+        while(j < sizeof(c)) {
+            A[k] = c[j];
+            j++;
+            k++;
+        }
+    }
+    else{
+        while(i < sizeof(b)) {
+            A[k] = b[i];
+            i++;
+            k++;
+        }
+    }
+}
+
+void mergeSort(int A[], int low, int high){
+    if(low < high){
+        int middle = (low+high)/2;
+        mergeSort(A, low, middle);
+        mergeSort(A, middle+1,high);
+        merge(A, low, middle, high);
+    }
+}
+
+int main(){
+    int arr[1000] = {};
+    int SS[1000];
+    int BS[1000];
+    int MS[1000];
+    int QS[1000];
+    //Creating a random array.
+    for(int i = 0; i < 1000; i++) {
+        arr[i] = (rand()%2000 + (-1000));
+    }
+    //creating copies of random array for each sorting array.
+    for (int j = 0; j < 1000; ++j) {
+        SS[j] = arr[j];
+        BS[j] = arr[j];
+        MS[j] = arr[j];
+        QS[j] = arr[j];
+    }
+
+    auto ssTime = SelectionSort(SS, 1000);
+    cout << "New array after Selection Sort: ";
+    for(int a = 0; a < 1000; a++){
+        cout << SS[a] << " ";
+    }
+    cout << endl;
+    cout << "SelectionSort time: " << ssTime.count() << " Milliseconds"<< endl;
+    cout << "SelectionSort lines of code: 10" << endl;
+    cout << "------------------------------------------------------------------" << endl;
+    auto bsTime = BubbleSort(BS, 1000);
+    cout << "New array after Bubble Sort: ";
+    for(int b = 0; b < 1000; b++){
+        cout << BS[b] << " ";
+    }
+    cout << endl;
+    cout << "BubbleSort time: " << bsTime.count() << " Milliseconds"<< endl;
+    cout << "BubbleSort lines of code: 6" << endl;
+    cout << "------------------------------------------------------------------" << endl;
+    auto start = high_resolution_clock::now();
+    mergeSort(MS,0, (sizeof(MS) - 1));
+    auto end = high_resolution_clock::now();
+    auto msDuration = duration_cast<milliseconds>(end - start);
+    cout << "New array after Merge Sort: ";
+    for(int c = 0; c < 1000; c++){
+        cout << MS[c] << " ";
+    }
+    cout << endl;
+    cout << "MergeSort time: " << msDuration.count() << " Milliseconds"<< endl;
+    cout << "MergeSort lines of code: 33" << endl;
+    return 0;
+}
